6ee02b4d61dae7c9ad5783f3f0d96b4f16ca871b
usmanasad87 <usmanasad0324@gmail.com>
2024-03-09 19:54:34 +0500

    complete app

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
+import 'screens/qibla_compass.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Qiblah',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      darkTheme: ThemeData.dark().copyWith(),
+      themeMode: ThemeMode.system,
+      home: Scaffold(
+        body: FutureBuilder(
+          future: FlutterQiblah.androidDeviceSensorSupport(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                ),
+              );
+            }
+
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error.toString()}'),
+              );
+            }
+            if (snapshot.hasData) {
+              return const QiblaCompass();
+            } else {
+              return const Text('Error');
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
